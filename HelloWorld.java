daf4c2415daf6e0e3e33e71163d54ab1d36dc7e2
srinuchowdary123 <95613471+srinuchowdary123@users.noreply.github.com>
2022-07-18 09:31:07 +0530

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -34,6 +34,6 @@
  */
 public class HelloWorld {
   public static void main(String[] args) {
-    System.out.println("good morning srinu how are you ==============");
+    System.out.println("good morning srinu welcome to jenkins ==============");
   }
 }
